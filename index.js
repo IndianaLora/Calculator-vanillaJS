269376be290c8899a93754a9b1e09616b1d6de3f
Indiana Lora <indiana@ensaludybelleza.com>
2021-06-20 11:11:13 -0400

    function to calculate the operation

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var operation;
+var operation = [];
+
 var input = document.getElementById("firstInput");
 var equal = document.getElementById("equal");
+var erase = document.getElementById("erase");
 
 function getValue(event) {
   var value = event;
   value = value.innerText;
-  input.value = input.append(value);
-
-  equal.addEventListener("click", () => {
-    operation = input.innerText;
-    var eval = operation;
-    console.log(eval);
-  });
+  value = input.append(value);
+  value = input.innerText;
+  operation.push(value);
 }
-function showInput() {}
+equal.addEventListener("click", () => {
+  var position = operation.length - 1;
+  var lastOp = operation[position];
+  var result = Math.round(eval(lastOp))
+  input.innerHTML = result;
+});
+
+erase.addEventListener("click", () => {
+  input.innerText = "";
+});
