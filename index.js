3a874ab85f744d315f19b07321cd76ca46f1d2f9
Indiana Lora <indiana@ensaludybelleza.com>
2021-06-20 17:00:45 -0400

    created a new away to calculate

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 var operation = [];
 
 var input = document.getElementById("firstInput");
+var prevOp = document.getElementById("prevOp");
 var equal = document.getElementById("equal");
 var erase = document.getElementById("erase");
 
 function getValue(event) {
   var value = event;
   value = value.innerText;
-  value = input.append(value);
-  value = input.innerText;
+  value = prevOp.append(value);
+  value = prevOp.innerText;
   operation.push(value);
 }
+erase.addEventListener("click", () => {
+  input.innerText = "";
+});
+
 equal.addEventListener("click", () => {
   var position = operation.length - 1;
   var lastOp = operation[position];
-  var result = Math.round(eval(lastOp))
-  input.innerHTML = result;
-});
 
-erase.addEventListener("click", () => {
-  input.innerText = "";
+  var includesSubstraction = lastOp.includes("-");
+  if (includesSubstraction) {
+    var firstValue = lastOp.split("-")[0];
+    var secondValue = lastOp.split("-")[1];
+    var result = firstValue - secondValue;
+    input.innerHTML = result;
+  }
+  var includesSum = lastOp.includes("+");
+  if (includesSum) {
+    var firstValue = lastOp.split("+")[0];
+    var secondValue = lastOp.split("+")[1];
+    var result = parseFloat(firstValue) + parseFloat(secondValue);
+    input.innerHTML = result;
+  }
+  var includesSum = lastOp.includes("/");
+  if (includesSum) {
+    var firstValue = lastOp.split("/")[0];
+    var secondValue = lastOp.split("/")[1];
+    var result = parseFloat(firstValue) / parseFloat(secondValue);
+    input.innerHTML = result;
+  }
+  var includesSum = lastOp.includes("*");
+  if (includesSum) {
+    var firstValue = lastOp.split("*")[0];
+    var secondValue = lastOp.split("*")[1];
+    var result = parseFloat(firstValue) * parseFloat(secondValue);
+    input.innerHTML = result;
+  }
 });
