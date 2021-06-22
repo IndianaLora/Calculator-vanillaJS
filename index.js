f29e805c714c33759b532df6705bf8f93964305c
Indiana Lora <indiana@ensaludybelleza.com>
2021-06-22 15:33:50 -0400

    program calculator so it only takes on operation at time

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var operation = [];
+var isOperation = false;
 var eraseOne = [];
+var a = [];
 var eraseOneDigit = document.getElementById("eraseOneDigit");
+
+var firstValue;
+var secondValue;
+var result;
 
 var input = document.getElementById("firstInput");
 var prevOp = document.getElementById("prevOp");
 var equal = document.getElementById("equal");
 var erase = document.getElementById("erase");
+
+var operators = document.querySelectorAll(".op");
 
 function getValue(event) {
   var value = event;
@@ -21,31 +29,49 @@ erase.addEventListener("click", () => {
 eraseOneDigit.addEventListener("click", () => {
   eraseOne = prevOp.innerText;
   eraseOne = eraseOne.toString().slice(0, -1);
-  console.log(eraseOne);
   prevOp.innerHTML = eraseOne;
 });
-prevOp.addEventListener("", () => {
-  //make that i doesnt that more than operation per time
-  //if includes in the 3rd position
-  //add event to --<-- button
-  //
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    isOperation = true ? !isOperation : isOperation;
+    if (isOperation == true && operation.length > 1) {
+      operation.pop();
+      var position = operation.length - 1;
+      var lastOp = operation[position];
+      console.log(lastOp);
+      prevOp.innerText = lastOp;
+      equalTo();
+    }
+  });
 });
-//Calculations
-equal.addEventListener("click", () => {
+equal.addEventListener("click", equalTo);
+
+function equalTo() {
   var position = operation.length - 1;
   var lastOp = operation[position];
+
   //-
-  var includesSubstraction = lastOp.includes("-");
-  if (includesSubstraction) {
-    var firstValue = lastOp.split("-")[0];
-    var secondValue = lastOp.split("-")[1];
-    var result = parseFloat(firstValue) - parseFloat(secondValue);
+  var includesSubs = lastOp.includes("-");
+  if (includesSubs) {
+    substraction();
+  }
+
+  function substraction() {
+    firstValue = lastOp.split("-")[0];
+    secondValue = lastOp.split("-")[1];
+    result = parseFloat(firstValue) - parseFloat(secondValue);
     input.innerHTML = result;
     prevOp.innerHTML = result;
   }
   //+
   var includesSum = lastOp.includes("+");
+
   if (includesSum) {
+    sum();
+  }
+
+  function sum() {
     var firstValue = lastOp.split("+")[0];
     var secondValue = lastOp.split("+")[1];
     var result = parseFloat(firstValue) + parseFloat(secondValue);
@@ -53,8 +79,13 @@ equal.addEventListener("click", () => {
     prevOp.innerHTML = result;
   }
   //÷
-  var includesSum = lastOp.includes("/");
-  if (includesSum) {
+  var includesDiv = lastOp.includes("/");
+
+  if (includesDiv) {
+    div();
+  }
+
+  function div() {
     var firstValue = lastOp.split("/")[0];
     var secondValue = lastOp.split("/")[1];
     var result = parseFloat(firstValue) / parseFloat(secondValue);
@@ -62,22 +93,52 @@ equal.addEventListener("click", () => {
     prevOp.innerHTML = result;
   }
   //*
-  var includesSum = lastOp.includes("*");
-  if (includesSum) {
+
+  var includesMul = lastOp.includes("*");
+  if (includesMul) {
+    mult();
+  }
+
+  function mult() {
     var firstValue = lastOp.split("*")[0];
     var secondValue = lastOp.split("*")[1];
     var result = parseFloat(firstValue) * parseFloat(secondValue);
     input.innerHTML = result;
     prevOp.innerHTML = result;
   }
-  var includesSum = lastOp.includes("%");
-  if (includesSum) {
+  //^
+  var includesPercent = lastOp.includes("^");
+
+  if (includesPercent) {
+    pow();
+  }
+
+  function pow() {
+    var firstValue = lastOp.split("^")[0];
+    var secondValue = lastOp.split("^")[1];
+    var result = parseFloat(firstValue) % parseFloat(secondValue);
+    result = Math.pow(firstValue, secondValue);
+    input.innerHTML = result;
+    prevOp.innerHTML = result;
+  }
+  //%
+  var includesPer = lastOp.includes("%");
+  if (includesPer) {
+    percent();
+  }
+
+  function percent() {
     var firstValue = lastOp.split("%")[0];
     var secondValue = lastOp.split("%")[1];
     var result = parseFloat(firstValue) % parseFloat(secondValue);
     input.innerHTML = result;
     prevOp.innerHTML = result;
   }
+  nan();
+  infinity();
+}
+
+function nan() {
   if (isNaN(prevOp.innerHTML)) {
     prevOp.innerHTML = "Invalid";
     input.innerHTML = "Invalid";
@@ -86,11 +147,14 @@ equal.addEventListener("click", () => {
       prevOp.innerHTML = "";
     }, 1000);
   }
-  if (result == "Infinity") {
+}
+function infinity() {
+  if (prevOp.innerHTML == "Infinity" && input.innerHTML == "Infinity") {
     input.innerHTML = "Cant divid by zero";
     prevOp.innerHTML = "Cant divid by zero";
+    setTimeout(() => {
+      input.innerHTML = "";
+      prevOp.innerHTML = "";
+    }, 1000);
   }
-  if (input.innerHTML == "" || prevOp.innerHTML == "") {
-    //do something
-  }
-});
+}
